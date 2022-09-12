6e281d1879d7b488fe3332cb6e5202b66dc94f79
temathc <temathc@yandex.ru>
2022-09-12 22:51:35 +0300

    top10

--- main.go
+++ main.go
@@ -3,32 +3,64 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
-type User struct {
-	Id   int
-	Name string
-	Age  int
-}
+// type User struct {
+// 	Id   int
+// 	Name string
+// 	Age  int
+// }
 
-func (u User) GetName() string {
-	return u.Name
-}
-func (u User) PrintName() {
-	fmt.Println(u.Name)
-}
+// func (u User) GetName() string {
+// 	return u.Name
+// }
+// func (u User) PrintName() {
+// 	fmt.Println(u.Name)
+// }
 
-type User2 struct {
-	User
-	id2 int
-}
+// type User2 struct {
+// 	User
+// 	id2 int
+// }
 
 func main() {
-	u1 := User{11, "oleg", 18}
-	fmt.Println(u1.GetName())
-	var ss User2
-	fmt.Println(ss.User.GetName())
+	// u1 := User{11, "oleg", 18}
+	// fmt.Println(u1.GetName())
+	//var ss User2
+	//fmt.Println(ss.User.GetName())
 	// еще раз посмотреть на использование структуры внудри другой структуры.....
+
+	d := "cat and dog, one dog,two cats and one man cat and dog, one dog,two cats and one man"
+	// sss := strings.Fields(d)
+	// sort.Slice(sss, func(i, j int) bool { return sss[i] < sss[j] })
+	// // for i := range sss {
+
+	// // }
+	// s2 := make(map[string]int)
+
+	// for _, v := range sss {
+	// 	_, vv := s2[v]
+	// 	if vv {
+	// 		s2[v] += 1
+	// 	} else {
+	// 		s2[v] = 1
+	// 	}
+
+	// }
+	// //fmt.Println(s2)
+
+	// fmt.Println(regexp.MustCompile(sss[2]))
+
+	tempStr := strings.Fields(d)
+	fmt.Println(tempStr)
+	m := make(map[string]int)
+	for _, word := range tempStr {
+		m[word]++
+		fmt.Println(m, "\n---")
+	}
+	//fmt.Println(m)
+
 }
 
 func Pars() {
